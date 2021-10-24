b0e2bd5af98a080e4876441d194097315bcb26f8
maziar.rumiani <maziar.rumiani@gmail.com>
2021-10-24 05:33:03 +0330

    fix hiding the strikethrough

--- showOptions.js
+++ showOptions.js
@@ -1,20 +1,24 @@
+let show = true;
 document.addEventListener('click',(e)=>{
+    
     document.querySelectorAll('.deleteItem').forEach((element)=>{
         element.style.visibility = 'hidden'
     })
     document.querySelectorAll('.strikethrough').forEach((element)=>{
         element.style.visibility = 'hidden'
     })
+
     let el = e.target.nextSibling;
-    if(e.target.classList.contains('newTask') ){
+    if(e.target.classList.contains('newTask') && show){
 
         el.style.visibility = 'visible'
         el.nextSibling.style.visibility = 'visible'
-
+        
         e.target.style.paddingRight = '50px';
         // e.target.style.background = 'red';
     }
-    else if(el){
+    else{
         // el.style.paddingRight = '10px';  
     }
+    show = !show
   })
