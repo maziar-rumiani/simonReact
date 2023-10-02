132668e0c7cc0eed0b09148882179b7008729ea5
rumiani <maziar.rumiani@gmail.com>
2023-10-02 15:06:51 +0330

    PWA feature added

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 import {loadTasks} from "./handlers/loadTasks.js";
 import {addTask} from "./modules/add.js";
 
+// Check if the browser supports service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js')
+      .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+          console.error('Service Worker registration failed:', error);
+      });
+}
+
+// Check if the app is installed
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   e.preventDefault();
+//   // const installButton = document.getElementById('install-button');
+//   // installButton.style.display = 'block';
+
+//   installButton.addEventListener('click', () => {
+//       e.prompt();
+//       installButton.style.display = 'none';
+//   });
+// });
+
+///////////////////////
 loadTasks()
 
 document.querySelector(".input").addEventListener('input', (e) => {
