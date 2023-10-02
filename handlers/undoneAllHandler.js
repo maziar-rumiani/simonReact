123d1086c344f91c27d5e190464e8c765c4438d4
rumiani <maziar.rumiani@gmail.com>
2023-10-02 15:44:10 +0330

    strikethrough on undoneAll fiexed

--- handlers/undoneAllHandler.js
+++ handlers/undoneAllHandler.js
@@ -1,11 +1,17 @@
-import {updateStatisticsHandler} from "./updateStatisticsHandler.js";
+import { updateStatisticsHandler } from "./updateStatisticsHandler.js";
 
 export const undoneAllHandler = (e) => {
-    let tasks = JSON.parse(localStorage.getItem("tasks"));
-    tasks.forEach( task => task.status = "undone" );
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    document.querySelectorAll(".done").forEach( el => el.classList.remove("done") );
-    document.querySelectorAll(".done").forEach( el => el.classList.add("undone") );
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks.forEach((task) => (task.status = "undone"));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  document.querySelectorAll(".done").forEach((el) => {
+    el.classList.remove("done");
+    el.classList.add("undone");
+  });
 
-    updateStatisticsHandler();
-  }
+  document.querySelectorAll(".strikethrough").forEach((el) => {
+    el.classList.remove("fas", "fa-check-square");
+    el.classList.add("far", "fa-circle");
+  });
+  updateStatisticsHandler();
+};
